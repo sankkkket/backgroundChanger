d26c0151de7e7a8289e7a8f2bb4a792fc33a2b1b
sankkkket <sanket.bachchaw7001@gmail.com>
2024-06-02 21:38:42 +0530

    test this app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("black")
+
+  let myStyle = {
+    backgroundColor: color
+  }
+
+  return (
+    <>
+    <div className='bodyy' style={myStyle}>
+      <div  className='color-btn'>
+        <button onClick={()=> setColor("red")} className='color-btnn btn-btn' type="button" class="btn btn-outline-primary">Red</button>
+        <button onClick={()=> setColor("green")} className='color-btnn' type="button" class="btn btn-outline-primary">Green</button>
+        <button onClick={()=> setColor("blue")} className='color-btnn' type="button" class="btn btn-outline-primary">Blue</button>
+        <button onClick={()=> setColor("orange")} className='color-btnn' type="button" class="btn btn-outline-primary">Orange</button>
+        <button onClick={()=> setColor("olive")} className='color-btnn' type="button" class="btn btn-outline-primary">Olive</button>
+        <button onClick={()=> setColor("grey")} className='color-btnn' type="button" class="btn btn-outline-primary">Grey</button>
+      </div>
+    </div>  
+    </>
+  )
+}
+
+export default App
